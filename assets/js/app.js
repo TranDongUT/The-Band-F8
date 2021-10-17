649a5132d90f91fcd052fd3b1392c1a29623f891
Tran Dong <tdn.1907@gmail.com>
2021-10-17 15:26:44 +0700

    official

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,14 +8,13 @@ const slider = $('#slider');
 const textSliders = $$('.text-silder');
 let amoutOfSlider = textSliders.length;
 let slideBegin = 0;
-console.log($(".text-silder.active"));
 
 setInterval(function(){
     if(slideBegin < amoutOfSlider){
         $(".text-silder.active").classList.remove("active");
         textSliders[slideBegin].classList.add('active');
         Object.assign(slider.style,{
-            background : `url('/assets/img/slider/${slideBegin+=1}.jpg')`,
+            background : `url("/assets/img/slider/s${slideBegin+=1}.jpg")`,
             backgroundSize : "contain",
             backgroundRepeat : "no-repeat",
         })
@@ -46,8 +45,53 @@ modalCloseBtn.onclick = function(){
 
 modal.onclick = function(){
     modal.classList.remove('show');
+    
 }
 
 modalContainer.onclick = function(e){
     e.stopPropagation();
+}
+
+
+        /* mobile - menu click */
+const menuBtn = $(".menu-btn");
+const header = $("#header");
+const content = $("#content");
+const navItems = $$("#nav li");
+const subNav = $("#nav li:last-child");
+const subNavItems = $('.sub-nav');
+console.log(subNavItems.clientHeight);
+
+
+
+navItems.forEach(function(e,index){
+    e.onclick = function(){
+        if(index != 4){
+            header.style.height = headerHeight + 'px';
+        }
+        else{
+            subNav.onclick = function(){
+                if(header.clientHeight == 228){
+                    header.style.height = 336 + 'px';
+                }
+                else{
+                     header.style.height = 228 + 'px'
+                }
+            }
+        }
+    }
+})
+
+content.onclick = function(){
+    header.style.height = headerHeight + 'px';
+}
+let headerHeight = header.clientHeight;
+menuBtn.onclick = function(){
+    if(header.clientHeight == headerHeight){
+        header.style.height = 228 + 'px'
+    }
+    else{
+        header.style.height = headerHeight + 'px';
+    }
+    
 }
